90d1637d9ec174ebd286bbb87e6f8329575a1571
Muhammad Rumi <muhammadrumi471@gmail.com>
2023-07-25 22:53:57 +0500

    completed exercise 3 + linted accordingly

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
-#include "bit_ops.h"
+// Copyright [2023] <Rumi>
+#include "bit_ops.h"  // NOLINT
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
     /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    return !!(x & (1u << n)); /* UPDATE WITH THE CORRECT RETURN VALUE*/
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
-void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+void set_bit(unsigned int *x, unsigned int n, unsigned int v) {
+    *x = ((*x & ~(1u << n)) | (v << n));
 }
+
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
     /* YOUR CODE HERE */
+    *x ^= (1 << n);
 }
 
--- lab02/bit_ops.h
+++ lab02/bit_ops.h
@@ -1,3 +1,9 @@
+#ifndef  LAB02_BIT_OPS_H_
+#define  LAB02_BIT_OPS_H_
+
+#include <stdio.h>
+#include "bit_ops.c"  // NOLINT
 unsigned get_bit(unsigned x, unsigned n);
 void set_bit(unsigned * x, unsigned n, unsigned v);
 void flip_bit(unsigned * x, unsigned n);
+#endif  // LAB02_BIT_OPS_H_
